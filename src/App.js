8bef98fd4ffde461e767302e7d78527d5db025ec
DanielValeriote <danielvaleriote@outlook.com>
2022-03-14 23:04:59 -0300

    create option component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import './App.css';
-
+import './App.scss';
+import logo from "./images/logo.svg";
+import triangleBg from "./images/bg-triangle.svg";
+import paperIcon from "./images/icon-paper.svg";
+import scissorIcon from "./images/icon-scissors.svg";
+import rockIcon from "./images/icon-rock.svg";
+import Option from './components/Option';
+import './styles/header.scss';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="logo" />
+        <div className="score-container">
+          <h5>score</h5>
+          <h2>12</h2>
+        </div>
       </header>
+      <main>
+        <section className="options-container">
+          {/* <div className="triangle-bg">
+            <img src={triangleBg} alt="triangle background" />
+          </div> */}
+          <Option
+            className="secondImage"
+            image={paperIcon}
+            name="paper"
+            color="hsl(230, 89%, 62%)"
+            id={1}
+          />
+          <Option
+            image={scissorIcon}
+            name="scissor"
+            color="hsl(39, 89%, 49%)"
+            id={2}
+          />
+          <Option
+            image={rockIcon}
+            name="rock"
+            color="hsl(349, 71%, 52%)"
+            id={3}
+          />
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/Option.js
@@ -0,0 +1,17 @@
+import "./Option.scss";
+
+const Option = ({image, name, color, id}) => {
+  return (
+    <div
+      className="option"
+      key={id}
+      style={{ border: `10px solid ${color}` }}
+    >
+      <div className={`option-image`}>
+        <img src={image} alt={name} />
+      </div>
+    </div>
+  );
+}
+
+export default Option
